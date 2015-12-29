3ac9cbde02cd7d5492701060824fd67191232f80
mamaddeveloper <mr.ashkan7786@gmail.com>
2015-12-29 18:56:32 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,14 +1,15 @@
 do
 
   function run(msg, matches)
-    return 'Merbot\n'..VERSION..'\nGitHub: https://git.io/v4Oi0\nLicense: GNU GPL v2'
+    return 'Fuzuli Nakon!!\nNo Fuzuli!!!\nNo Fuzuli!!!'
   end
 
   return {
     description = 'Shows bot version',
     usage = '!version: Shows bot version',
     patterns = {
-      '^!version$'
+      '^[/!@#?]([Vv]ersion)$',
+      '^([Vv]ersion)$'
     },
     run = run
   }
